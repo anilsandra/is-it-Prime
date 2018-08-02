f5b1253cb5ed9b84304bd0c4f7da867a9bbf02fb
Anil <iamanilsandra@gmail.com>
2018-08-02 12:21:09 -0400

    Is It Prime
    This app tells us the number which we entered is prime or not?

--- is it Prime/ViewController.swift
+++ is it Prime/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtInput: UITextField!
+    @IBOutlet var oputLabel: UILabel!
+    
+    @IBAction func button(_ sender: Any) {
+        
+        if let userString = txtInput.text {
+            let num = Int(userString)
+            if let userNum = num {
+            var isPrime = true
+            if userNum == 1{
+                isPrime = false
+            }
+            var i = 2
+            while i < userNum{
+                if userNum % i == 0{
+                isPrime = false
+                }
+                i = i + 1
+            }
+                if isPrime{
+                    oputLabel.text = "Yes, \(userNum) is Prime!"
+                }else{
+                    oputLabel.text = "Nope, \(userNum) isnt a Prime!"
+                }
+            
+            
+        }else{
+            oputLabel.text="Please enter number"
+        }
+        }
+    }
+    @IBOutlet var eLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
